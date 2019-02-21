a97997360e2bacbd5f4fb8c9e643f1ffb4a32ab1
Dmitry Sichkar <dmmeteo@gmail.com>
2019-02-21 19:04:51 +0200

    :rocket: First deploy
    Success first deploy on aws

--- handler.js
+++ handler.js
@@ -4,10 +4,25 @@ module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Go Serverless v1.0! Your function "hello" executed successfully!',
       input: event,
     }),
   };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.helloOld = async (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function "helloOld" executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
