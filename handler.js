aa21b407859e08acd59563a9bc1155455887d17f
Dmitry Sichkar <dmmeteo@gmail.com>
2019-02-22 12:45:49 +0200

    :wrench: Add entrypoints
    Set entrypoints with methods

--- handler.js
+++ handler.js
@@ -14,11 +14,19 @@ module.exports.hello = async (event, context) => {
 };
 
 module.exports.helloOld = async (event, context, callback) => {
+  const device = (headers) => {
+    if (headers['CloudFront-Is-Desktop-Viewer']) return 'Desktop'
+    if (headers['CloudFront-Is-SmartTV-Viewer']) return 'SmartTV'
+    if (headers['CloudFront-Is-Mobile-Viewer']) return 'Mobile'
+    if (headers['CloudFront-Is-Tablet-Viewer']) return 'Tablet'
+  }
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function "helloOld" executed successfully!',
-      input: event,
+      your_country: event.headers['CloudFront-Viewer-Country'],
+      your_device: device(event.headers)
     }),
   };
 
