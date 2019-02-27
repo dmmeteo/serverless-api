716873b5edca90d8573f1639e523d8b7b756ae39
Dmitry Sichkar <dmmeteo@gmail.com>
2019-02-27 23:39:30 +0200

    :construction: Fixing python handler
    Can't fix python handler on python3.7

--- py_handler.py
+++ py_handler.py
@@ -3,9 +3,7 @@
 
 def hello(event, context):
     return {
-        "statusCode": 200,
-        "body": "<html><body><p>Hello from python3.7!</p></body></html>",
-        "headers": {
-            "Content-Type": "text/html"
-        }
+        'status': 200, 
+        'body': 'Lambda executed successfully!'
     }
+
